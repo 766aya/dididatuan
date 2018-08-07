5e40e40b23063cc2e1c58335a9780977a4b907f0
766aya <1494233259@qq.com>
2018-08-08 00:19:59 +0800

    端口修改 代理80

--- config/index.js
+++ config/index.js
@@ -19,8 +19,8 @@ module.exports = {
 
     // Various Dev Server settings
     host: '0.0.0.0', // can be overwritten by process.env.HOST
-    port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: false,
+    port: 8888, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
