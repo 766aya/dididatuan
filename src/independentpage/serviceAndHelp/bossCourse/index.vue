fb8fd6fa9ce995aa33fad1c3ebae6c82df74ee00
766aya <1494233259@qq.com>
2018-08-11 14:02:59 +0800

    update

--- src/independentpage/serviceAndHelp/bossCourse/index.vue
+++ src/independentpage/serviceAndHelp/bossCourse/index.vue
@@ -1,19 +1,42 @@
 <template>
     <div id="bossCourse">
-        大神教程
+        <div class="content"></div>
+        <div class="btn" @click="understood">我已了解</div>
     </div>
 </template>
 
 <script>
+    import { Dialog, Toast } from 'vant';
+
     export default {
         name: 'bossCourse',
         data() {
             return {
-
+                createdDate: new Date().getTime(),
+            }
+        },
+        methods: {
+            understood() {
+                let endDate = new Date().getTime();
+                let interval = endDate - this.createdDate
+                if (interval < 5000) {
+                    Toast('浏览时长不足！')
+                } else {
+                    Toast('已完成！')
+                }
             }
         }
     }
 </script>
 
 <style scoped>
+    .btn {
+        width: 100vw;
+        height: 60px;
+        font-size: 1.25rem;
+        color: #FFFFFF;
+        background: #FF6600;
+        text-align: center;
+        line-height: 60px;
+    }
 </style>
--- src/independentpage/serviceAndHelp/headCourse/index.vue
+++ src/independentpage/serviceAndHelp/headCourse/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="headCourse">
-        团长教程
+        <div class="content"></div>
+        <div class="btn" @click="understood">我已了解</div>
     </div>
 </template>
 
@@ -11,9 +12,29 @@
             return {
 
             }
+        },
+        methods: {
+            understood() {
+                let endDate = new Date().getTime();
+                let interval = endDate - this.createdDate
+                if (interval < 5000) {
+                    Toast('浏览时长不足！')
+                } else {
+                    Toast('已完成！')
+                }
+            }
         }
     }
 </script>
 
 <style scoped>
+    .btn {
+        width: 100vw;
+        height: 60px;
+        font-size: 1.25rem;
+        color: #FFFFFF;
+        background: #FF6600;
+        text-align: center;
+        line-height: 60px;
+    }
 </style>
