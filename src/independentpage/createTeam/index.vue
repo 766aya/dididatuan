f7330cb959e015367065fde8e9e81a74dcdb2f07
766aya <1494233259@qq.com>
2018-08-13 21:31:09 +0800

    login and datuan update

--- src/independentpage/createTeam/index.vue
+++ src/independentpage/createTeam/index.vue
@@ -3,27 +3,23 @@
         <div class="title">订单信息</div>
         <div class="item">
             <div class="lside">角色</div>
-            <div class="rside">{{info.role}}</div>
+            <div class="rside" v-text="info.role"></div>
         </div>
         <div class="item">
             <div class="lside">副本</div>
-            <div class="rside">{{info.fuben}}</div>
+            <div class="rside" v-text="info.fuben"></div>
         </div>
         <div class="item">
             <div class="lside">原价</div>
-            <div class="rside">{{info.Oprice}}</div>
+            <div class="rside" v-text="`￥${info.Oprice}元`">{{}}</div>
         </div>
         <div class="item">
             <div class="lside">优惠券</div>
-            <div class="rside" @click="isCouponShow = true">{{info.Coupon}}<span data-v-7b579e8e="" class="text t2 iconfont icon-you"></span></div>
+            <div class="rside" @click="isCouponShow = true">{{info.Coupon}}<span class="text iconfont icon-you"></span></div>
         </div>
         <div class="item">
             <div class="lside">优惠金额</div>
-            <div class="rside">{{info.amount}}</div>
-        </div>
-        <div class="item">
-            <div class="lside">需支付</div>
-            <div class="rside">{{info.price}}</div>
+            <div class="rside" v-text="`￥${info.amount}元`"></div>
         </div>
         <div class="title">支付方式</div>
         <div class="pay" @click="payWay=1">
@@ -39,7 +35,7 @@
             <div class="rside"><van-icon name="success" class="rside-icon" v-show="payWay==3" /></div>
         </div>
         <van-submit-bar
-          disabled
+          :disabled="isSubmit"
           :price="price"
           button-text="去付款"
           @submit="onSubmit"
@@ -62,27 +58,43 @@
         data() {
             return {
                 info: {
-                    role: '角色',
+                    role: '123',
                     fuben: '副本',
                     Oprice: '原价',
-                    Coupon: '优惠券',
-                    amount: '优惠金额',
-                    price: '需支付'
+                    Coupon: '请选择优惠券',
+                    amount: 100
                 },
                 payWay: 1,
                 coupon: this.$store.state.coupon,
                 chosenCoupon: -1,
                 isCouponShow: false,
-                price: 4000
+                isSubmit: false
+            }
+        },
+        computed: {
+            price() {
+                return this.$route.query.fubenPrice - this.info.amount
             }
         },
         created() {
+            this.info.role = this.$route.query.roleName;
+            this.info.fuben = this.$route.query.fubenName;
+            this.info.Oprice = this.$route.query.fubenPrice/100;
+        },
+        watch: {
+            chosenCoupon(newVal, oldVal) {
+                console.log(newVal, oldVal)
+            }
         },
         methods: {
             onChange (index) {
                 this.chosenCoupon = index;
                 this.isCouponShow = false
                 console.log("coupon: ", this.coupon)
+                if (index === -1) {
+                    this.info.Coupon = '不使用优惠券'
+                    return
+                }
                 this.info.Coupon = this.coupon.list[index].name
             },
             onSubmit () {
@@ -93,37 +105,46 @@
 </script>
 
 <style scoped>
-#createTeam {
-    display: grid;
-    grid-auto-rows: 50px;
-}
-.item {
-    display: grid;
-    grid-template-columns: auto auto;
-    background-color: #fff;
-    border-bottom: 1px solid #5d5d5d;
-    padding: 0 15px;
-}
-.title {
-    line-height: 50px;
-    padding: 0 15px;
-}
-.pay {
-    display: grid;
-    grid-template-columns: auto 40px;
-    background-color: #fff;
-    border-bottom: 1px solid #5d5d5d;
-    padding: 0 15px;
-}
-.lside {
-    line-height: 50px;
-}
-.rside {
-    line-height: 50px;
-    text-align: right;
-}
-.rside-icon {
-    color: #47c808;
-    font-weight: 700;
-}
+    #createTeam {
+        display: grid;
+        grid-auto-rows: 50px;
+    }
+    .item {
+        display: grid;
+        grid-template-columns: auto auto;
+        background-color: #fff;
+        border-bottom: 1px solid #F5F5F5;
+        padding: 0 15px;
+        font-size: 0.85rem;
+    }
+    .title {
+        line-height: 50px;
+        font-size: 1rem;
+        font-weight: 700;
+        padding: 0 15px;
+    }
+    .pay {
+        display: grid;
+        grid-template-columns: auto 40px;
+        background-color: #fff;
+        font-size: 0.85rem;
+        border-bottom: 1px solid #F5F5F5;
+        padding: 0 15px;
+    }
+    .lside {
+        line-height: 50px;
+    }
+    .rside {
+        line-height: 50px;
+        text-align: right;
+        color: #5D5D5D;
+        font-size: 0.85rem;
+    }
+    .text {
+        font-size: 0.8rem;
+    }
+    .rside-icon {
+        color: #47c808;
+        font-weight: 700;
+    }
 </style>
