816cdd4a2b724157d90f1e5db6f4400b2c3aa120
huzhengw115 <huzhengw115@qq.com>
2018-08-16 21:03:31 +0800

    diditeam

--- src/independentpage/serviceAndHelp/didiTeam/index.vue
+++ src/independentpage/serviceAndHelp/didiTeam/index.vue
@@ -1,6 +1,9 @@
 <template>
     <div id="didiTeam">
-        滴滴交流群
+        <h2 class="title">欢迎加入交流群</h2>
+        <div class="list" v-for="(item, index) in list" key="index">
+            {{item.title}}：{{item.number}}
+        </div>
     </div>
 </template>
 
@@ -9,11 +12,25 @@
         name: 'didiTeam',
         data() {
             return {
-
+                list: [{
+                    title: '交流1群',
+                    number: '787778909'
+                }]
             }
         }
     }
 </script>
 
 <style scoped>
+#didiTeam {
+    margin-top: 15px;
+    background-color: #fff;
+    height: 100vh;
+    display: grid;
+    grid-auto-rows: 50px;
+    padding: 15px;
+}
+.list {
+    font-size: 0.85em;
+}
 </style>
